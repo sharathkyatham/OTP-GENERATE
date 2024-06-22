08c73d500b34feabf68dfe96149bc256476581f2
Your Name <you@example.com>
2024-06-22 12:06:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Otp from './component/Otp';
+
+function App() {
+  return (
+    
+  <div>
+  <h1>GENERATING OTP</h1> 
+  <div className='preloader'></div>
+  <div className='span'><span>Loading.......</span>X</div>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/component/Otp.js
@@ -0,0 +1,9 @@
+const generateOTP = () => {
+    const length = 6;
+    let otp = "";
+    for (let i = 0; i < length; i++){
+        otp += Math.floor(Math.random() *10);
+    }
+    return otp;
+} 
+console.log(generateOTP());
